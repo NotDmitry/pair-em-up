8ba74fcd8f5c2ce5b904dd0274c9f962de617ad9
Dzmirty Buldyha <buldygodmitrij@gmail.com>
2025-11-16 19:09:50 +0300

    feat: add game screen return button styles

--- pair-em-up/js/game-screen.js
+++ pair-em-up/js/game-screen.js
@@ -1,18 +1,27 @@
 export function getGameScreen(mode, backBtnCallback) {
   const gameScreen = document.createElement('div');
   const header = document.createElement('header');
-  const backBtn = document.createElement('button');
+  const roundBtn = document.createElement('button');
+  const roundBtnIcon = document.createElement('img');
   const title = document.createElement('h1');
   const main = document.createElement('main');
   const field = document.createElement('div');
   const cell = document.createElement('button');
 
+  roundBtn.classList.add('button', 'button_round');
+  roundBtnIcon.classList.add('button__icon');
+
+  // Return button
+  const backBtn = roundBtn.cloneNode(true);
+  const backBtnIcon = roundBtnIcon.cloneNode(true);
+  backBtnIcon.src = './assets/svg/return.svg';
+  backBtnIcon.alt = 'Return icon';
+  backBtn.append(backBtnIcon);
   backBtn.addEventListener('click', () => {
     backBtnCallback();
   })
 
   title.append(document.createTextNode(mode));
-
 
   header.append(backBtn, title);
   gameScreen.append(header, main);
